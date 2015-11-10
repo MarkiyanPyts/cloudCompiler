45cc84e284672a4b0069206ccb28bf3b38ab74bd
Markiyan Pyts (mpyts) <markiyan.pyts@osf-global.com>
2015-11-10 02:41:03 +0200

    make adjustmends for compile command

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports =
     "repoDir": "D:\\GoogleDrive\\OSF\\MyProjects\\Demandware\\Loreal\\LORA\\Urban Decay HK\\codebace\\ecom-lancome-au\\cartridges\\app_lancome_au\\cartridge\\static\\default",
     "gitPushRemote": "origin",
     "gitPushBranch": "master",
-    "serverUrl": "localhost",
+    "serverUrl": "91.210.21.144",
     "gitClonePath": "git@bitbucket.org:Markiyan_Pyts/testrepoforcompiler.git",
     "cloudCommands": "gulp compass"
 }
